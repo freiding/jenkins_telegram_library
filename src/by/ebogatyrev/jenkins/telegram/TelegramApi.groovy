665dfb4b6783b7421acdfb45b4e4af9d8729f8d8
Eugene Bogatyrev <bogatyrev.eugene@gmail.com>
2023-02-18 22:30:34 +0300

    cleanup

--- src/by/ebogatyrev/jenkins/telegram/TelegramApi.groovy
+++ src/by/ebogatyrev/jenkins/telegram/TelegramApi.groovy
@@ -4,34 +4,19 @@ import java.net.URLEncoder;
 
 class TelegramApi {
     def botToken;
-    def isPipeline;
 
     TelegramApi(token) {
-        TelegramApi(token, false)
-    }
-
-    TelegramApi(token, isPipeline) {
         this.botToken = token;
-        this.isPipeline = isPipeline;
     }
 
     def sendMessage(String chatId, String message) {
         def _message = URLEncoder.encode(message, "UTF-8")
         def cmd = "curl -s -X POST https://api.telegram.org/bot$botToken/sendMessage -d chat_id=$chatId -d text=$_message"
-        shell cmd
+        cmd.execute()
     }
 
     def sendFile(String chatId, String filePath) {
         def cmd =  "curl -v -F chat_id=$chatId -F document=@$filePath https://api.telegram.org/bot$botToken/sendDocument"
-        shell cmd
+        cmd.execute()
     }
-
-    private def shell(String cmd) {
-        if (isPipeline) {
-            sh cmd
-        } else {
-            cmd.execute()
-        }
-    }
-
 }
